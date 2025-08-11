eca81982c04601eee048368835177033788571c8
Eduardo Ceretta <duduceretta@gmail.com>
2025-08-11 17:53:29 -0300

    Add threading examples for Java and Python
    Added ThreadComParametros.java and thread_com_parametros.py demonstrating list population and sorting using threads in Java and Python, respectively. Minor cleanup in JFrame_Principal.java to remove unused imports and suppress warnings.

---
+++ codigosAula/Python/thread_com_parametros.py
@@ -0,0 +1,62 @@
+import threading
+import random
+import time
+ 
+def popular_lista_intervalo(lista, tamanho_lista):
+    for _ in range(tamanho_lista):
+        lista.append( random.randint(1000,10000) )
+    
+ 
+def ordenar_bolha(lista):
+    houve_troca = True
+    while houve_troca:
+        houve_troca = False
+        for i in range(len(lista) - 1):
+            if lista[i] > lista[i + 1]:
+                lista[i], lista[i + 1] = lista[i + 1], lista[i]
+                houve_troca = True
+                
+    print('Bolha finalizado...')
+ 
+def ordenar_pente(lista):
+    print(lista)
+    
+    n = len(lista)
+    distancia = n
+    fator = 1.3
+    houve_troca = True
+ 
+    while distancia > 1 or houve_troca:
+        distancia = int(distancia / fator)
+        if distancia < 1:
+            distancia = 1
+        houve_troca = False
+ 
+        for i in range(n - distancia):
+            if lista[i] > lista[i + distancia]:
+                lista[i], lista[i + distancia] = lista[i + distancia], lista[i]
+                houve_troca = True
+    
+    print('Pente finalizado...')
+ 
+ 
+if __name__ == "__main__":
+    lista1 = []
+    lista2 = []
+ 
+    popula_lista1 = threading.Thread(target=popular_lista_intervalo,args=(lista1, 10000))
+    popula_lista2 = threading.Thread(target=popular_lista_intervalo,args=(lista2, 10000))
+ 
+    popula_lista1.start()
+    popula_lista2.start()
+    
+    popula_lista1.join()
+    popula_lista2.join()
+ 
+    print(f"Listas populadas.")
+     
+    ordena_pente = threading.Thread(target=ordenar_pente,args=(lista2,))
+    ordena_bolha = threading.Thread(target=ordenar_bolha,args=(lista1,))
+ 
+    ordena_bolha.start()
+    ordena_pente.start()
